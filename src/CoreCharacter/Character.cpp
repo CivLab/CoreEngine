86857fbce766c6276ebc3e2b9c77c1700f0e7b4e
Elzed7 <elb.0702@gmail.com>
2018-08-29 23:20:19 +0200

    [add] basic methods related to characters

--- src/CoreCharacter/Character.cpp
+++ src/CoreCharacter/Character.cpp
@@ -2,11 +2,37 @@
 
 
 
-Character::Character()
+Character::Character() : Character("")
 {
+}
+
+Character::Character(std::string name, int life, int damage)
+{
+	m_name = name;
+	m_ID = name;
+	m_life = life;
+	m_maxLife = life;
+	m_damage = damage;
+	m_statut = ALIVE;
 }
 
 
 Character::~Character()
 {
+}
+
+void Character::updateStatut()
+{
+	m_statut = m_life > 0 ? ALIVE : DEAD;
+}
+
+void Character::isHit(int damage)
+{
+	m_life = (fmax(0, m_life - damage));
+	updateStatut();
+}
+
+void Character::isHealed(int heal)
+{
+	m_life = fmin(m_maxLife, m_life + heal);
 }
--- src/CoreCharacter/Character.h
+++ src/CoreCharacter/Character.h
@@ -1,8 +1,37 @@
 #pragma once
+#include "Weapon.h"
+#include <iostream>
+
 class Character
 {
 public:
 	Character();
+	Character(std::string name, int life = 100, int damage = 10);
+
 	~Character();
+
+	enum Statut
+	{
+		DEAD = 0,
+		ALIVE = 1,
+		REMOVED = 2
+	};
+
+	bool isAlive() { return m_statut == ALIVE; }
+
+	void updateStatut();
+	void isHit(int damage);
+	void isHealed(int heal);
+
+	int getLife() { return m_life; }
+	int getMaxLife() { return m_maxLife; }
+	
+private:
+	std::string m_ID;
+	std::string m_name;
+	Statut m_statut;
+	int m_life;
+	int m_maxLife;
+	int m_damage;
 };
 

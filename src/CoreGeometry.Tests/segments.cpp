abe3ea1a11e33d16612c41415eb05dff79468bac
Elzed7 <elb.0702@gmail.com>
2018-08-30 00:00:14 +0200

    [add] templates for tests

---
+++ src/CoreGeometry.Tests/segments.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include "../CoreGeometry/segment.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CoreGeometryTests
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Your test code here
+		}
+
+	};
+}

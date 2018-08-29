891bdb8572d73d3641b195abf7bb3d6a01b37140
Elzed7 <elb.0702@gmail.com>
2018-08-30 00:01:01 +0200

    [add] template for MovingElement class methods

--- src/CoreStructure/MovingElement.cpp
+++ src/CoreStructure/MovingElement.cpp
@@ -10,3 +10,22 @@ MovingElement::MovingElement()
 MovingElement::~MovingElement()
 {
 }
+
+void MovingElement::move(float timeUnits)
+{
+	Point* pDirection = &getDirection();
+	float speed = getSpeed();
+	move(pDirection, timeUnits, speed);
+}
+
+void MovingElement::move(Point * direction, float timeUnits)
+{
+	float speed = getSpeed();
+	move(direction, timeUnits, speed);
+}
+
+void MovingElement::move(Point * direction, float timeUnits, float speed)
+{
+	float distance = timeUnits * speed;
+	updatePosition(*direction*distance);
+}
--- src/CoreStructure/MovingElement.h
+++ src/CoreStructure/MovingElement.h
@@ -7,9 +7,18 @@ public:
 	MovingElement();
 	~MovingElement();
 
-	bool updatePosition(float timeLapse);
-	bool updateSpeed(Point* newSpeed);
+	void rotate(float alpha);
+	void rotate(float alphaPerTimeUnit, float timeUnits);
+
+	void move(float timeUnits = 1);
+	void move(Point* direction, float timeUnits = 1);
+	void move(Point* direction, float timeUnits, float speed);
+
+	bool updatePosition(Point newPosition, bool teleport = false);
+	bool updateSpeed(float newSpeed);
 	bool updateDirection(Point* newDirection);
+
+	float getSpeed() { return m_speed; }
 
 private:
 	float m_speed;

dc3e2933fa82c81aea8972afe4a49466f1fcdc69
Elzed7 <elb.0702@gmail.com>
2018-08-30 00:00:38 +0200

    [add] template for Weapon class methods

--- src/CoreCharacter/Weapon.cpp
+++ src/CoreCharacter/Weapon.cpp
@@ -6,6 +6,13 @@ Weapon::Weapon()
 {
 }
 
+Weapon::Weapon(std::string name, int damage, int firingRate)
+{
+	m_name = name;
+	m_damage = damage;
+	m_firingRate = firingRate;
+}
+
 
 Weapon::~Weapon()
 {
--- src/CoreCharacter/Weapon.h
+++ src/CoreCharacter/Weapon.h
@@ -1,8 +1,17 @@
 #pragma once
+
+#include <iostream>
+
 class Weapon
 {
 public:
 	Weapon();
+	Weapon(std::string name, int damage = 10, int firingRate = 1);
 	~Weapon();
+
+private:
+	std::string m_name;
+	int m_damage;
+	int m_firingRate;
 };
 
